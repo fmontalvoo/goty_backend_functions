fa8db0220a47eab2985b4b5babf5c6526b1269fb
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-05-17 15:45:38 -0500

    Se hace consulta de juegos a firebase.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,27 @@
+import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
+
+admin.initializeApp();
+
+const db = admin.firestore();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 // export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
+//     functions.logger.info("Hello logs!", { structuredData: true });
+//     response.send("Hello from Firebase!");
 // });
+
+export const getGOTY = functions.https.onRequest(async (request, response) => {
+    const ref = db.collection("goty");
+    const snapshot = await ref.get();
+    const goty = snapshot.docs.map((doc) => {
+        return {
+            ...doc.data(),
+            id: doc.id,
+        };
+    }
+    );
+    response.json(goty);
+});
